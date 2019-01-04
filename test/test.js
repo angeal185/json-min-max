0b17c7f8bd71877e6d1a8258fef8af43a475eb3c
angeal185 <beneaves01@hotmail.com>
2019-01-04 10:10:00 +0800

    init

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const JMM = require('../')
+
+let x = {
+  data:"result"
+},
+y = JSON.stringify(x),
+z = JSON.stringify(x,0,2);
+
+console.log("beautify:" + JMM.beautify(y,2))
+console.log("minify:" + JMM.minify(z))
